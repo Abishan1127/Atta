3a6b38262cd4356cd985be1fc5b9171d90247405
Abishan1127 <kunananthan19abishan.com>
2025-07-03 15:48:59 +0530

    team card

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -6,7 +6,7 @@ const TeamCard = ({ member }) => {
     <div className="card h-100 shadow rounded-0 overflow-hidden">
       <div className="position-relative">
         <img src={member.image} alt={member.name} className="card-img-top img-fluid" />
-        <div className="card-body bg-white text-center rounded shadow position-relative mb-4 team-cardbody" >
+        <div className="card-body bg-white text-center rounded position-relative team-cardbody" >
           <h5 className="card-title fw-bold mb-1">{member.name}</h5>
           <p className="text-danger mb-3">{member.title}</p>
           <div className="d-flex justify-content-center gap-2">
